1844130f6cfbe102b641ce293916a3d93219f815
Jean-Daniel KLEIN <kleinjeandaniel@gmail.com>
2025-08-03 22:24:45 +0200

    changement des rules eslint

--- packages/types/src/zod/auth/hearthBeat.schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const heartbeatSchema = z.object({
-  userId: z.string(),
-  lastSeen: z.date(),
-});
-
-export type HeartbeatSchema = z.infer<typeof heartbeatSchema>;
